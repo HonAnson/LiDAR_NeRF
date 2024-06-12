cafdf8ac47fb2d718dd761a9e67027c7f7dc7026
HonAnson <99588976+HonAnson@users.noreply.github.com>
2024-06-12 20:40:25 +0100

    Initial Attempt to reconstruction (#1)
    * update commit
    
    * testing commit
    
    * drafting loss function
    
    * sigmoid approach update
    
    * first attempt on training
    
    * debugging model
    
    * changing sampling strategy
    
    * updated notes
    
    * fixed bug in indexing
    
    * first reconstruction
    
    * added upsampling for small angle rays
    
    * updated training function to include upsampling
    
    * added upsampling
    
    * rebased main changes

--- memo.md
+++ memo.md
@@ -2,4 +2,51 @@
 
 - X, Y, Z, are location of the point, with respect to the camera centre
 - No time information provided
-- No 
+- No
+
+
+
+### Trial 0
+- Used spherical harmonics as encoder for view direction
+- Combined measurement from 5 frames into 1 set, assuming linear translation between frames only
+- Model architecture with 8 layers of 256 fully connectted neurons
+- Tried L2 and L1 loss
+- Did not generalize well
+
+
+### Trial 1
+- Use angular encoding same as nerf paper
+- Loss function same as SHINE mapping's sigmoid mapping design
+- Used sampled position and view direction for each ray as input
+- 8 layers of 512 fully connected neuron, with skip connection at between 4th and 5th later
+- Embedding dimension was 10 for both position and view direction
+- Sampling strategy of uniform sampling + pertebration w/ 100 bins from 3m distance to 75m from camera centre
+- Achieved Low loss function value of approximately 0.04, but fail to achieve anything reasonable upon visualization
+- Suspect reason due to majority of sampled position are belong positive or negative side of SDF, causing class imbalancing, and thus collaps of model
+- Suspect a bug in calculation of loss function
+ 
+### Trial 4
+- fixed indexing problem in sampling, which appears to be the cause of previous bug
+- Loss value converges at around 0.25 after 1 epoch, relatively high loss
+- Currently the sampling method samples mainly "on the ray", will try with a more balancd class in next trial
+- if next trial fails, will look into nerf in the wild, or other approach
+
+### Trial 5
+- loss convers around 0.2 after 1 epoch
+- Able to reconstruct the floor  
+- rough reconstruction of vehicles around
+- Poor reconstruction to structures
+- maybe consider space wrapping?
+
+### Trial 7
+- tunned hyperparameter of angle embedding dimension to 15
+- No significant improvement could be seen
+- Plan: do more literature review
+- explore oct-tree embedding instead of positional embedding instead (allow better representation efficiency)
+- explore upsampling for small angle 
+
+### Trial 8
+- updated sampling strategy so that denser points are sampled around points in point cloud
+
+
+
--- train.py
+++ train.py
@@ -10,19 +10,17 @@ import torch.optim as optim
 import math
 
 
-# load data from dataset directory, filterout irralevent datas
+# Load data from dataset directory, filterout irralevent datas
 def loadData():
     # Specify the directory path
     dataset_path = 'datasets/testing1'
-
     # List all files in the specified path, ignoring directories
     files = [f for f in os.listdir(dataset_path) if os.path.isfile(os.path.join(dataset_path, f))]
     files.sort()
-
     # read the files
     points_xyz = []
     for s in files:
-        path = 'datasets/testing1/' + s
+        path = dataset_path + s
         df = pd.read_csv(path)
         a = df.to_numpy()
         b = a[:,8:11]
@@ -30,10 +28,7 @@ def loadData():
     return points_xyz   # type = list
 
 
-### we now process the data
-# (1) Convert data to spherical coordinate
-# (2) Filter out extreme value datas
-# 3. Stack them into one big numpy matrix
+### Process the data
 def prepareData(points_xyz):
     # (1) Convert to spherical coorindate
     # NOTE: points in spherical coordinate are arranged: [r, elev, pan]
@@ -51,105 +46,57 @@ def prepareData(points_xyz):
     t3 = np.array([-2.661,-0.263,0.166])
     t4 = np.array([-3.607,-0.156,0.039])
     translations = [t0, t1, t2, t3, t4]
-
-    # camera centre locations
-    centres = [-t for t in translations]
-    centres_data = []
-    for i,c in enumerate(centres):
-        l = len(points_sphere[i])
-        temp = np.tile(c, (l, 1))
-        centres_data.append(temp)
-
-    # (3) Stack camera centre, r and angles into a matrix
-    stacked = []
-    for i in range(len(points_sphere)):
-        temp = np.hstack((points_sphere[i], centres_data[i]))
-        stacked.append(temp)
-
-    dataset = np.array([])
-    for i in range(len(stacked)):
-        if i == 0:
-            dataset = stacked[i]
-        else:
-            dataset = np.vstack((dataset, stacked[i]))
-    np.random.shuffle(dataset)
-
-    # (2) Filter out points where the distance value is = 0
-    mask1 = dataset[:, 0] != 0
-    mask2 = dataset[:,0] > 50
-    mask = mask1 + mask2
-    dataset = dataset[mask]
     return dataset   # type = numpyArray
+
+
+
+
+class LiDAR_NeRF(nn.Module):
+    def __init__(self, embedding_dim_pos = 10, embedding_dim_dir = 4, hidden_dim = 256):
+        super(LiDAR_NeRF, self).__init__()
+        self.embedding_dim_dir = embedding_dim_dir
+        self.embedding_dim_pos = embedding_dim_pos
+        self.layers = nn.Sequential(
+            nn.Linear(embedding_dim_pos * 3 + embedding_dim_dir*2, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),               
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),               
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),               
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),               
+            nn.Linear(512, 1)          # Output layer with 1 output
+        )
+        
+    @staticmethod
+    def positional_encoding(x, L):
+        out = [x]
+        for j in range(L):
+            out.append(torch.sin(2 ** j * x))
+            out.append(torch.cos(2 ** j * x))
+        return torch.cat(out, dim=1)
+
+    def forward(self, o, d):
+        emb_x = self.positional_encoding(o, self.embedding_dim_pos)
+        emb_d = self.positional_encoding(d, self.embedding_dim_dir)
+        
+        return self.layers(x)
+
+
+    def lossBCE(self, r, y, sampled_pos):  # r = distance from lidar measurement, y = function output at different points, it is a vector
+        y_sigmoid_value = nn.sigmoid(y)
+        r_sigmoid_value = nn.sigmoid(-sampled_pos-r)
+        loss = nn.CrossEntropyLoss(r_sigmoid_value, y_sigmoid_value)
+        return loss
+
+
+
+
+
+
 
 
 
 ### Train the model
 def trainModel(dataset):
-
-    features = 5
-    batch_size = 256
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"Using {device} device")
-
-    # Convert dataset to pytorch tensor
-    X = np.array(dataset[:,1:])
-    y = np.array(dataset[:,0])
-
-    # Convert to tensor:
-    X_tensor = torch.from_numpy(X).double()
-    y_tensor = torch.from_numpy(y).double()
-
-    dataset = TensorDataset(X_tensor, y_tensor)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-    # Now we prepare to train the model
-
-    class MLP(nn.Module):
-        def __init__(self):
-            super(MLP, self).__init__()
-            self.layers = nn.Sequential(
-                nn.Linear(5, 512),  # Input layer with 5 inputs and 10 outputs
-                nn.ReLU(),                # Activation function
-                nn.Linear(512, 512),        # Hidden layer with 512 neurons
-                nn.ReLU(),                # Activation function
-                nn.Linear(512, 512),        # Hidden layer with 512 neurons
-                nn.ReLU(),                # Activation function
-                nn.Linear(512, 512),        # Hidden layer with 512 neurons
-                nn.ReLU(),                # Activation function
-                nn.Linear(512, 512),        # Hidden layer with 512 neurons
-                nn.ReLU(),                # Activation function
-                nn.Linear(512, 1)          # Output layer with 1 output
-            )
-            
-        def forward(self, x):
-            return self.layers(x)
-
-    # Initialize the model
-    model = MLP().to(device)
-
-    # Loss and Optimizer
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
-
-    # Training Loop
-    num_epochs = 50
-    for epoch in range(num_epochs):
-        for inputs, targets in dataloader:
-            inputs, targets = inputs.to(device).float(), targets.to(device).float()
-            
-            # Forward pass
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        print(loss)
-
-
+        
     return
 
 
@@ -165,13 +112,20 @@ def cart2sph(xyz):
     elev = np.arctan2(list(z), np.sqrt(list(XsqPlusYsq)))
     pan = np.arctan2(list(x), list(y))
     output = np.array([r, elev, pan])
-    return rearrange(output, 'a b -> b a') 
+    return rearrange(output, 'a b -> b a') #take transpose
 #######################################
 
 
 
 
+
+
+
 if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using {device} device")
     points = loadData()
-    dataset = prepareData(points)
-    trainModel(dataset)
+
+
+
+
