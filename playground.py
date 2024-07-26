4b2c0efc34ad924e4ac1a56123e988eec2eb4d93
HonAnson <hon.anson@gmail.com>
2024-07-26 15:58:18 +0100

    also created dataset for training euclidean model

--- playground.py
+++ playground.py
@@ -12,23 +12,40 @@ from numpy import cos, sin, sqrt, arctan2, array
 import torch.nn as nn
 from einops import rearrange
 ### fucking around
-from preprocess import loadDataFromRegisteredSlam
+from preprocess import loadDataFromRegisteredSlam, getDistanceAndDirection, prepareTrainingData
 from train import getSpacing, getTargetCumulativeTransmittance, getTargetTerminationDistribution
 
-variance = 0.1
-t, delta = getSpacing(2, 1000)
-T = getTargetCumulativeTransmittance(t, variance=variance)
-h = getTargetTerminationDistribution(T, delta, variance=variance)
 
-T[0,-1] = 0
-temp  = sum((T[0,:] * (1 - T[0,:]))*delta[0,:])
+name = r'round_plant2'
+input_path = r'datasets/registered/' + name + r'.json'
+data = loadDataFromRegisteredSlam(input_path)
+temp = prepareTrainingData(data)
+
+quickVizNumpy(temp[200000:600000,:])
 breakpoint()
-x = np.array(t[0,:])
-y = np.array()
 
-plt.plot(x, k*(1-k))
-plt.title("Plot of NumPy Array")
-plt.xlabel("x-axis")
-plt.ylabel("y-axis")
-plt.show()
+
+
+
+
+
+
+
+
+# variance = 0.1
+# t, delta = getSpacing(2, 1000)
+# T = getTargetCumulativeTransmittance(t, variance=variance)
+# h = getTargetTerminationDistribution(T, delta, variance=variance)
+
+# T[0,-1] = 0
+# temp  = sum((T[0,:] * (1 - T[0,:]))*delta[0,:])
+# breakpoint()
+# x = np.array(t[0,:])
+# y = np.array()
+
+# plt.plot(x, k*(1-k))
+# plt.title("Plot of NumPy Array")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+# plt.show()
 
--- preprocess.py
+++ preprocess.py
@@ -52,43 +52,47 @@ def prepareTrainingData(data):
     scene_points = np.zeros((1,3)) # for scaling point cloud
     iter = 0
     total_iter = len(keys)
+    x = np.zeros((1,3))
     for key in keys:
         pcd_cart = np.array(data[key]['point_cloud'])
-        ray_origin = np.array(data[key]['pose'])
-        scene_points = np.vstack((scene_points, pcd_cart, ray_origin))     # for scaling the scene
-        distance, ray_direction = getDistanceAndDirection(pcd_cart, ray_origin)
 
+        x = np.vstack((x,pcd_cart))
 
-        n = distance.shape[0]
-        ray_origin_tiled = np.tile(ray_origin, (n,1))
-        training_data = np.hstack((ray_origin_tiled,ray_direction, distance))
-        output = np.vstack((output, training_data))
-        if iter % 50 == 0:
-            message = f"Preparing data ... ({iter}/{total_iter})"
-            printProgress(message)
-        iter += 1
+    #     ray_origin = np.array(data[key]['pose'])
+    #     scene_points = np.vstack((scene_points, pcd_cart, ray_origin))     # for scaling the scene
+    #     distance, ray_direction = getDistanceAndDirection(pcd_cart, ray_origin)
 
-    # scale furthest distance between two points (including camera position) to 1        
-    scene_points = scene_points[1:,:]   # discard first row
-    max_xyz = np.max(scene_points, axis=0)
-    min_xyz = np.min(scene_points, axis=0)
-    max_distance = np.max(max_xyz - min_xyz)
-    output[:,0:3] /= max_distance       # normalize camera position
-    output[:,-1] /= max_distance        # normalize depth
-    print(f"\nScaling factor is: {1 / max_distance}")
+    #     n = distance.shape[0]
+    #     ray_origin_tiled = np.tile(ray_origin, (n,1))
+    #     training_data = np.hstack((ray_origin_tiled,ray_direction, distance))
+    #     output = np.vstack((output, training_data))
+    #     if iter % 50 == 0:
+    #         message = f"Preparing data ... ({iter}/{total_iter})"
+    #         printProgress(message)
+    #     iter += 1
 
-    return output[1:,:]
+    # # scale furthest distance between two points (including camera position) to 1        
+    # scene_points = scene_points[1:,:]   # discard first row
+    # max_xyz = np.max(scene_points, axis=0)
+    # min_xyz = np.min(scene_points, axis=0)
+    # max_distance = np.max(max_xyz - min_xyz)
+    # output[:,0:3] /= max_distance       # normalize camera position
+    # output[:,-1] /= max_distance        # normalize depth
+    # print(f"\nScaling factor is: {1 / max_distance}")
+
+    # return output[1:,:]
+    return x
 
 
 
 if __name__ == "__main__":
-    name = r'round_plant2'
+    name = r'box_plant2'
     input_path = r'datasets/registered/' + name + r'.json'
     data = loadDataFromRegisteredSlam(input_path)
     training_data = prepareTrainingData(data)
     output_name = name + r'.npy'
-    np.save(output_name, training_data)
-    print(f"file have been saved to {output_name}")
+    # np.save(output_name, training_data)
+    # print(f"file have been saved to {output_name}")
     
 
 
